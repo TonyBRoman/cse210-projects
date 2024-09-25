ac8c0de26e0007ddbefd8e05ee6040d81b7b5c72
TonyBRoman <tonybaggon@gmail.com>
2024-09-25 14:30:13 -0600

    while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,20 +7,25 @@ class Program
         Console.WriteLine("What is the magic number?: ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("What is your guess?: ");
-        int guess = int.Parse(Console.ReadLine());
+        int guess = 0;
 
-        if (guess == magicNumber)
+        while (guess != magicNumber)
         {
+            Console.WriteLine("What is your guess?: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == magicNumber)
+            {
             Console.WriteLine("You guessed it!");
-        }
-        else if (guess > magicNumber)
-        {
+            }
+            else if (guess > magicNumber)
+            {
             Console.WriteLine("Lower");
-        }
-        else 
-        {
+            }
+            else 
+            {
             Console.WriteLine("Higher");
-        }
+            }
+        }  
     }
 }
