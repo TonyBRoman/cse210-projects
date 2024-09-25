8e19eeedd9210eff9b1012f052e57e7a982c6103
TonyBRoman <tonybaggon@gmail.com>
2024-09-25 14:42:10 -0600

    number of guesses

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -8,15 +8,19 @@ class Program
         int magicNumber = random.Next(1,101);
 
         int guess = 0;
+        int attempts = 0;
 
         while (guess != magicNumber)
         {
             Console.WriteLine("What is your guess?: ");
             guess = int.Parse(Console.ReadLine());
 
+            attempts++;
+
             if (guess == magicNumber)
             {
             Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {attempts} guesses.");
             }
             else if (guess > magicNumber)
             {
