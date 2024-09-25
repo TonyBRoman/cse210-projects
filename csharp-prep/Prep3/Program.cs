8ba9a9fb78d41f84fbe59ba043d82013631064d5
TonyBRoman <tonybaggon@gmail.com>
2024-09-25 14:57:12 -0600

    do you want to play again?

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,32 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random random = new Random();
-        int magicNumber = random.Next(1,101);
-
-        int guess = 0;
-        int attempts = 0;
-
-        while (guess != magicNumber)
+        string playAgain = "yes";
+        while (playAgain.ToLower() == "yes") 
         {
-            Console.WriteLine("What is your guess?: ");
-            guess = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            int magicNumber = random.Next(1,101);
 
-            attempts++;
+            int guess = 0;
+            int attempts = 0;
 
-            if (guess == magicNumber)
+            while (guess != magicNumber)
             {
-            Console.WriteLine("You guessed it!");
-            Console.WriteLine($"It took you {attempts} guesses.");
-            }
-            else if (guess > magicNumber)
-            {
-            Console.WriteLine("Lower");
-            }
-            else 
-            {
-            Console.WriteLine("Higher");
-            }
-        }  
+                Console.Write("What is your guess?: ");
+                guess = int.Parse(Console.ReadLine());
+
+                attempts++;
+
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {attempts} guesses.");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else 
+                {
+                    Console.WriteLine("Higher");
+                }
+            }  
+
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine(); 
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
 }
