58e9e808e00d8115dbbef0be8c8b8dbb6574aa22
TonyBRoman <tonybaggon@gmail.com>
2024-10-29 19:57:34 -0600

    MathAssignment class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -7,5 +7,10 @@ class Program
         Assigment assigment = new Assigment("Samuel Bennet", "Multiplication");
 
         Console.WriteLine(assigment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
+
