07c14be654a277c085902c44e3f175d6a81f4ac9
TonyBRoman <tonybaggon@gmail.com>
2024-11-02 23:35:18 -0600

    Exercise Tracking

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,131 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 4.8),
+            new Cycling("03 Nov 2022", 30, 15.0),
+            new Swimming("03 Nov 2022", 30, 20)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
+
+public class Activity
+{
+    private string _date;
+    protected int _minutes;  
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_date} ({_minutes} min): Distance {GetDistance():0.00} km, Speed {GetSpeed():0.00} kph, Pace {GetPace():0.00} min per km";
+    }
+}
+
+public class Running : Activity
+{
+    private double _distance;
+
+    public Running(string date, int minutes, double distance) : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (_distance / _minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return _minutes / _distance;
+    }
+}
+
+public class Cycling : Activity
+{
+    private double _speed;  
+
+    public Cycling(string date, int minutes, double speed) : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return _speed * (_minutes / 60.0);
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
+}
+
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int minutes, int laps) : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * 50 / 1000.0;
+    }
+
+    public override double GetSpeed()
+    {
+        double distance = GetDistance();
+        return (distance / _minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        double distance = GetDistance();
+        return _minutes / distance;
+    }
+}
+
+
