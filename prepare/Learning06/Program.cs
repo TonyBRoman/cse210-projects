0d8eb722d5c14dd1a8c124678251122520ec1961
TonyBRoman <tonybaggon@gmail.com>
2024-11-02 18:18:47 -0600

    List

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,11 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Square square = new Square("Blue", 5);
-        Console.WriteLine("Color: " + square.GetColor());
-        Console.WriteLine("Area: " + square.GetArea());
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4),
+            new Rectangle("Green", 3, 7),
+            new Circle("Yellow", 5)
+        };
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine("Color" + shape.GetColor());
+            Console.WriteLine("Area: ", + shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
